9ca947cfb4905713a39354e080a22b53342963f2
jessicato2002 <jessicato.2002@gmail.com>
2025-08-08 12:35:06 -0700

    made separate pages for Creator and actual Survey

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import SurveyCreatorWidget from './components/SurveyCreator';
-
+import SurveyComponent from './components/SurveyComponent';
 function App() {
   return (
     <div className="App">
-      <SurveyCreatorWidget></SurveyCreatorWidget>
+      <Routes>
+        <Route path="/" element={<SurveyCreatorWidget/>}></Route>
+        <Route path="/survey" element={<SurveyComponent/>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/SurveyComponent.tsx
@@ -0,0 +1,30 @@
+// components/Survey.tsx
+//this will render the survey created in Survey Creator
+
+import 'survey-core/survey-core.css';
+
+import {Model} from 'survey-core';
+import {Survey} from 'survey-react-ui'
+
+//need to import JSON saved in localstorage
+const defaultJson = {
+    elements: [{
+      name: "FirstName",
+      title: "Enter your first name:",
+      type: "text"
+    }, {
+      name: "LastName",
+      title: "Enter your last name:",
+      type: "text"
+    }]
+  }
+
+export default function SurveyComponent(){
+    //grabbing content saved to localstorage and saving it, else display default JSON data
+    const contentToRender = window.localStorage.getItem("survey-json") || JSON.stringify(defaultJson);
+    //Model is a constructor and you're creating an instance of Model and naming it survey
+    const survey = new Model(contentToRender);
+
+
+    return <Survey model={survey}/>;
+}
